1e8e9c1eaeadce09371c1d6cbd3e9f715ce2ad33
azerty9971 <doyen.gilles@gmail.com>
2024-03-22 17:24:32 +0100

    Update config_flow.py

--- config_flow.py
+++ config_flow.py
@@ -90,7 +90,7 @@ class TuyaConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             tuya_data = self.hass.data[DOMAIN_ORIG]
             for config in tuya_data:
                 config_entry = self.hass.config_entries.async_get_entry(config)
-                LOGGER.debug(f"config_entry -> {vars(config_entry)}")
+                LOGGER.debug(f"config_entry -> {vars(config_entry.data)}")
                 return self.async_create_entry(
                     title=config_entry.title,
                     data=config_entry.data,
