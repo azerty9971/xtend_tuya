98986f233689bae3c5d8f605446b8bbbedf121de
azerty9971 <doyen.gilles@gmail.com>
2024-06-20 11:24:03 +0200

    Update base.py

--- custom_components/xtend_tuya/base.py
+++ custom_components/xtend_tuya/base.py
@@ -45,32 +45,40 @@ class IntegerTypeData:
         """Return the step scaled."""
         return self.step / (10**self.scale)
 
-    def scale_value(self, value: float | int) -> float:
+    def scale_value(self, value: float) -> float:
         """Scale a value."""
+        if not isinstance(value, float):
+            value = float(value)
         return value / (10**self.scale)
 
-    def scale_value_back(self, value: float | int) -> int:
+    def scale_value_back(self, value: float) -> int:
         """Return raw value for scaled."""
+        if not isinstance(value, float):
+            value = float(value)
         return int(value * (10**self.scale))
 
     def remap_value_to(
         self,
         value: float,
-        to_min: float | int = 0,
-        to_max: float | int = 255,
+        to_min: float = 0,
+        to_max: float = 255,
         reverse: bool = False,
     ) -> float:
         """Remap a value from this range to a new range."""
+        if not isinstance(value, float):
+            value = float(value)
         return remap_value(value, self.min, self.max, to_min, to_max, reverse)
 
     def remap_value_from(
         self,
         value: float,
-        from_min: float | int = 0,
-        from_max: float | int = 255,
+        from_min: float = 0,
+        from_max: float = 255,
         reverse: bool = False,
     ) -> float:
         """Remap a value from its current range to this range."""
+        if not isinstance(value, float):
+            value = float(value)
         return remap_value(value, from_min, from_max, self.min, self.max, reverse)
 
     @classmethod
@@ -166,8 +174,7 @@ class TuyaEntity(Entity):
         *,
         prefer_function: bool = False,
         dptype: Literal[DPType.ENUM],
-    ) -> EnumTypeData | None:
-        ...
+    ) -> EnumTypeData | None: ...
 
     @overload
     def find_dpcode(
@@ -176,8 +183,7 @@ class TuyaEntity(Entity):
         *,
         prefer_function: bool = False,
         dptype: Literal[DPType.INTEGER],
-    ) -> IntegerTypeData | None:
-        ...
+    ) -> IntegerTypeData | None: ...
 
     @overload
     def find_dpcode(
@@ -185,8 +191,7 @@ class TuyaEntity(Entity):
         dpcodes: str | DPCode | tuple[DPCode, ...] | None,
         *,
         prefer_function: bool = False,
-    ) -> DPCode | None:
-        ...
+    ) -> DPCode | None: ...
 
     def find_dpcode(
         self,
