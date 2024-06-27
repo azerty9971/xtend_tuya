615a35a587be7af84c14266ff5a914f5bcee3cab
azerty9971 <doyen.gilles@gmail.com>
2024-06-27 20:12:40 +0200

    Update __init__.py

--- custom_components/xtend_tuya/__init__.py
+++ custom_components/xtend_tuya/__init__.py
@@ -80,11 +80,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: TuyaConfigEntry) -> bool
     for config_entry in tuya_data:
         if entry.title == config_entry.title:
             reuse_config = True
-            tuya_config_entry = hass.config_entries.async_get_entry(
-                config_entry.entry_id
-            )
-            LOGGER.warning(f"tuya_config_entry : {tuya_config_entry}")
-            tuya_device_manager = tuya_config_entry.manager
+            tuya_device_manager = config_entry.runtime_data.manager
             tuya_mq = tuya_device_manager.mq
             manager = DeviceManager(
                 TUYA_CLIENT_ID,
@@ -99,7 +95,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: TuyaConfigEntry) -> bool
             manager.scene_repository  = tuya_device_manager.scene_repository
             manager.user_repository   = tuya_device_manager.user_repository
             manager.set_overriden_device_manager(tuya_device_manager)
-            tuya_device_manager.remove_device_listener(tuya_config_entry.listener)
+            tuya_device_manager.remove_device_listener(config_entry.runtime_data.listener)
             tuya_mq.remove_message_listener(tuya_device_manager.on_message)
             break
     
