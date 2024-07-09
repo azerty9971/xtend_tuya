9824411acb51c49ada939310044b4d06b7116931
azerty9971 <doyen.gilles@gmail.com>
2024-07-10 00:41:29 +0200

    Update util.py

--- custom_components/xtend_tuya/util.py
+++ custom_components/xtend_tuya/util.py
@@ -34,12 +34,14 @@ def determine_property_type(type, value = None) -> DPType:
             return DPType(DPType.STRING)
 
 def prepare_value_for_property_update(dp_item, value) -> str:
-    LOGGER.warning(f"prepare_value_for_property_update => {dp_item} <=> {value}")
-    value_type = dp_item.get("valueType", None)
-    if value_type is not None:
-        if value_type == DPType.BOOLEAN:
-            if bool(value):
-                return "true"
-            else:
-                return "false"
+    #LOGGER.warning(f"prepare_value_for_property_update => {dp_item} <=> {value}")
+    config_item = dp_item.get("config_item", None)
+    if config_item is not None:
+        value_type = config_item.get("valueType", None)
+        if value_type is not None:
+            if value_type == DPType.BOOLEAN:
+                if bool(value):
+                    return "true"
+                else:
+                    return "false"
     return str(value)
