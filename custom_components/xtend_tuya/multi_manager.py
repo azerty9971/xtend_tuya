3523dc43ce7f15f7508c17f5f98521e758149c73
azerty9971 <doyen.gilles@gmail.com>
2024-07-19 03:01:45 +0200

    Update multi_manager.py

--- custom_components/xtend_tuya/multi_manager.py
+++ custom_components/xtend_tuya/multi_manager.py
@@ -102,7 +102,7 @@ class TuyaIntegrationRuntimeData(NamedTuple):
     generic_runtime_data: any
 
 
-class MultiManager:
+class MultiManager:  # noqa: F811
     def __init__(self, hass: HomeAssistant, entry: XTConfigEntry) -> None:
         self.sharing_account: TuyaSharingData = None
         self.iot_account: TuyaIOTData = None
@@ -233,9 +233,9 @@ class MultiManager:
         device_manager.add_device_listener(device_listener)
         return TuyaIOTData(device_manager=device_manager,mq=mq,device_ids=device_ids,device_listener=device_listener)
     
-    def update_device_cache(self):
+    async def update_device_cache(self):
         if self.sharing_account:
-            self.sharing_account.device_manager.update_device_cache()
+            await self.sharing_account.device_manager.update_device_cache()
     
     def is_device_in_domain_device_maps(self, domains: list[str], device_entry_identifiers: list[str]):
         if device_entry_identifiers[0] in domains:
