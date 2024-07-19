78945f8685314ed4b713d5e093f15078d5cf5f51
azerty9971 <doyen.gilles@gmail.com>
2024-07-19 03:23:22 +0200

    awaits

--- custom_components/xtend_tuya/__init__.py
+++ custom_components/xtend_tuya/__init__.py
@@ -69,6 +69,7 @@ async def update_listener(hass, entry):
 async def async_setup_entry(hass: HomeAssistant, entry: XTConfigEntry) -> bool:
     """Async setup hass config entry.""" 
     multi_manager = MultiManager(hass, entry)
+    await multi_manager.setup_entry(hass, entry)
 
     # Get all devices from Tuya
     try:
