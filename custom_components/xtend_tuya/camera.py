97b7bbf81c34ba9a3d1ce59ef8926a2d77cb8718
azerty9971 <doyen.gilles@gmail.com>
2024-07-21 14:06:06 +0200

    Fix errors

--- custom_components/xtend_tuya/camera.py
+++ custom_components/xtend_tuya/camera.py
@@ -38,7 +38,6 @@ async def async_setup_entry(
 
         async_add_entities(entities)
 
-    hass_data.manager.register_device_descriptors("carmeras", CAMERAS)
     async_discover_device(hass_data.manager, hass_data.manager.device_map)
 
     entry.async_on_unload(
