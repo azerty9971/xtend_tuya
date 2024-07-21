626e7ca1f30648199fc4cc886806edb914f9b821
azerty9971 <doyen.gilles@gmail.com>
2024-07-22 01:15:39 +0200

    Update xt_tuya_sharing.py

--- custom_components/xtend_tuya/xt_tuya_sharing.py
+++ custom_components/xtend_tuya/xt_tuya_sharing.py
@@ -214,7 +214,6 @@ class DeviceManager(Manager):
         super()._on_device_other(device_id, biz_code, data)
 
     def _read_code_value_from_state(self, device, state):
-        LOGGER.warning(f"_read_code_value_from_state => {device.local_strategy}")
         if "code" in state and "value" in state:
             return state["code"], state["value"]
         elif "dpId" in state and "value" in state:
