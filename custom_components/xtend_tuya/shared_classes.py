6b290a859a0dcda99538cccde1ec2a9408fa18c0
azerty9971 <doyen.gilles@gmail.com>
2024-07-22 00:35:28 +0200

    Update shared_classes.py

--- custom_components/xtend_tuya/shared_classes.py
+++ custom_components/xtend_tuya/shared_classes.py
@@ -1,11 +1,17 @@
 from __future__ import annotations
 from typing import Any
 
-class XTDeviceProperties:  # noqa: F811
+class XTDeviceProperties:
     local_strategy: dict[int, dict[str, Any]] = {}
     status: dict[str, Any] = {}
     function: dict[str, XTDeviceFunction] = {}
     status_range: dict[str, XTDeviceStatusRange] = {}
+
+    def __init__(self) -> None:
+        self.local_strategy = {}
+        self.status = {}
+        self.function = {}
+        self.status_range = {}
 
     def merge_in_device(self, device):
         if hasattr(device, "local_strategy"):
@@ -17,12 +23,12 @@ class XTDeviceProperties:  # noqa: F811
         if hasattr(device, "status_range"):
             device.status_range.update(self.status_range)
 
-class XTDeviceStatusRange:  # noqa: F811
+class XTDeviceStatusRange:
     code: str
     type: str
     values: str
 
-class XTDeviceFunction:  # noqa: F811
+class XTDeviceFunction:
     code: str
     desc: str
     name: str
