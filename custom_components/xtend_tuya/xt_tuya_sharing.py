a9501f29b37cf39287062515275333a58d9e7214
azerty9971 <doyen.gilles@gmail.com>
2024-07-22 16:24:40 +0200

    Fix error

--- custom_components/xtend_tuya/xt_tuya_sharing.py
+++ custom_components/xtend_tuya/xt_tuya_sharing.py
@@ -205,7 +205,7 @@ class DeviceManager(Manager):
         device = self.device_map.get(device_id, None)
         if not device:
             return
-        status_new = self.multi_manager.convert_device_report_status_list(status)
+        status_new = self.multi_manager.convert_device_report_status_list(device_id, status)
         status_new = self.multi_manager.apply_virtual_states_to_status_list(device, status_new)
         if self.other_device_manager:
             self.other_device_manager._on_device_report(device_id, status_new)
