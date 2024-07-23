a91dd19e88ab9e8b4a0e9f5f5c254c5d24bc439f
azerty9971 <doyen.gilles@gmail.com>
2024-07-23 16:26:38 +0200

    Add missing field

--- custom_components/xtend_tuya/xt_tuya_iot.py
+++ custom_components/xtend_tuya/xt_tuya_iot.py
@@ -175,6 +175,7 @@ class XTTuyaDeviceManager(TuyaDeviceManager):
         response = self.api.get(f"/v1.0/users/{self.api.token_info.uid}/devices")
         if response["success"]:
             for item in response["result"]:
+                LOGGER.warning(f"XTDevice => {item}")
                 device = XTDevice(**item)
                 status = {}
                 for item_status in device.status:
