f15efbdc5283d6cb9ebcd05ed741db50fa3f67dd
azerty9971 <doyen.gilles@gmail.com>
2024-07-25 02:36:27 +0200

    Update multi_manager.py

--- custom_components/xtend_tuya/multi_manager.py
+++ custom_components/xtend_tuya/multi_manager.py
@@ -429,8 +429,8 @@ class MultiManager:  # noqa: F811
         for virtual_state in virtual_states:
             if virtual_state.virtual_state_value == VirtualStates.STATE_COPY_TO_MULTIPLE_STATE_NAME:
                 for item in status:
-                    code, value = self._read_code_value_from_state(device, item)
-                    if code is not None and code == virtual_state.key:
+                    code, value, result_ok = self._read_code_value_from_state(device, item)
+                    if result_ok and code == virtual_state.key:
                         for state_name in virtual_state.vs_copy_to_state:
                             new_status = {"code": str(state_name), "value": value}
                             status.append(new_status)
@@ -449,11 +449,11 @@ class MultiManager:  # noqa: F811
                     device.status[virtual_state.key] = 0
                 if virtual_state.key in device.status:
                     for item in status:
-                        code, value = self._read_code_value_from_state(device, item)
-                        if code == virtual_state.key:
+                        code, value, result_ok = self._read_code_value_from_state(device, item)
+                        if result_ok and code == virtual_state.key:
                             item["value"] += device.status[virtual_state.key]
                             continue
-                        if code is None:
+                        if not result_ok:
                             for dict_key in item:
                                 dp_id = int(dict_key)
                                 dp_id_item = device.local_strategy.get(dp_id, None)
@@ -482,7 +482,7 @@ class MultiManager:  # noqa: F811
             for status in statuses:
                 devices = self._get_devices_from_device_id(dev_id)
                 for device in devices:
-                    code, value = self._read_code_value_from_state(device, status)
+                    code, value, result_ok = self._read_code_value_from_state(device, status)
                     #LOGGER.debug(f"status => {status}")
                     #LOGGER.debug(f"on_message ({source}) => code: {code}, value: {value}")
                     if code == "add_ele":
