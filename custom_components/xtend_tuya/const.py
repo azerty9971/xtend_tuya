47a9823060b05c0b416ac5e2bbe145e7dab9db88
azerty9971 <doyen.gilles@gmail.com>
2024-07-27 06:51:16 +0200

    Update const.py

--- custom_components/xtend_tuya/const.py
+++ custom_components/xtend_tuya/const.py
@@ -52,8 +52,8 @@ CONF_APP_TYPE = "tuya_app_type"
 TUYA_CLIENT_ID = "HA_3y9q4ak7g4ephrvke"
 TUYA_SCHEMA = "haauthorize"
 
-TUYA_DISCOVERY_NEW = "xt_tuya_discovery_new"
-TUYA_HA_SIGNAL_UPDATE_ENTITY = "xt_tuya_entry_update"
+TUYA_DISCOVERY_NEW = "tuya_discovery_new"
+TUYA_HA_SIGNAL_UPDATE_ENTITY = "tuya_entry_update"
 
 TUYA_RESPONSE_CODE = "code"
 TUYA_RESPONSE_MSG = "msg"
