195fa4351ec718ac768e5d4b431125ca42cf6f2b
azerty9971 <doyen.gilles@gmail.com>
2024-07-28 11:22:33 +0200

    Test decorator on Tuya regular class

---
+++ custom_components/xtend_tuya/tuya_decorators.py
@@ -0,0 +1,24 @@
+import functools
+from .const import (
+    LOGGER
+)
+from tuya_sharing import (
+    CustomerDevice,
+    Manager,
+    SharingDeviceListener,
+    SharingTokenListener,
+)
+
+def report_called(func, after: bool):
+    LOGGER.warning(f"{func.__name__} called (after = {after})")
+
+def wrapper(func):
+    @functools.wraps(func)
+    def wrapped(*args, **kwargs):
+        report_called(func, False)
+        return_val = func(*args, **kwargs)
+        report_called(func, True)
+        return return_val
+    
+def decorate_tuya_manager(tuya_manager: Manager):
+    tuya_manager.refresh_mq = wrapper(tuya_manager.refresh_mq)
