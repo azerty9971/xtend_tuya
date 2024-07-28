14c13abcb02604795168961e243266d32bb464f8
azerty9971 <doyen.gilles@gmail.com>
2024-07-28 12:26:27 +0200

    Update tuya_decorators.py

--- custom_components/xtend_tuya/tuya_decorators.py
+++ custom_components/xtend_tuya/tuya_decorators.py
@@ -18,9 +18,9 @@ from tuya_sharing import (
 def async_wrapper(func, callback):
     @functools.wraps(func)
     async def wrapped(*args, **kwargs):
-        callback(False)
+        callback(False, *args, **kwargs)
         return_val = await func(*args, **kwargs)
-        callback(True)
+        callback(True, *args, **kwargs)
         return return_val
     return wrapped
 
