f7c3a81df5f1abf2837ef4a65fa9c0299884ad84
azerty9971 <doyen.gilles@gmail.com>
2024-07-29 23:06:17 +0200

    Update multi_manager.py

--- custom_components/xtend_tuya/multi_manager.py
+++ custom_components/xtend_tuya/multi_manager.py
@@ -278,8 +278,8 @@ class MultiManager:  # noqa: F811
     def _get_available_device_maps(self) -> list[dict[str, XTDevice]]:
         return_list: list[dict[str, XTDevice]] = list()
         if self.sharing_account:
-            if other_manager := self.sharing_account.device_manager.get_overriden_device_manager():
-                return_list.append(other_manager.device_map)
+            """if other_manager := self.sharing_account.device_manager.get_overriden_device_manager():
+                return_list.append(other_manager.device_map)"""
             return_list.append(self.sharing_account.device_manager.device_map)
         if self.iot_account:
             return_list.append(self.iot_account.device_manager.device_map)
