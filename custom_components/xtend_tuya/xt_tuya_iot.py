794f626e716729d3367d51eca90081063ed849d9
azerty9971 <doyen.gilles@gmail.com>
2024-07-29 13:08:43 +0200

    Update xt_tuya_iot.py

--- custom_components/xtend_tuya/xt_tuya_iot.py
+++ custom_components/xtend_tuya/xt_tuya_iot.py
@@ -232,6 +232,7 @@ class XTTuyaDeviceManager(TuyaDeviceManager):
         if response2.get("success"):
             result = response2.get("result", {})
             model = json.loads(result.get("model", "{}"))
+            LOGGER.warning(f"{model}")
             device_properties.model = model
             for service in model["services"]:
                 for property in service["properties"]:
