0277ad0a3d25ca6937c2ee2d6c85cdb873535792
azerty9971 <doyen.gilles@gmail.com>
2024-08-05 01:44:19 +0200

    Update multi_manager.py

--- custom_components/xtend_tuya/multi_manager.py
+++ custom_components/xtend_tuya/multi_manager.py
@@ -476,6 +476,7 @@ class MultiManager:  # noqa: F811
         virtual_states = self.get_category_virtual_states(device.category)
         if device.id == "bf85bd241924094329wbx0":
             LOGGER.warning(f"apply_init_virtual_states BEFORE: {device.status}")
+            LOGGER.warning(f"Virtual States : {virtual_states}")
         for virtual_state in virtual_states:
             if virtual_state.virtual_state_value == VirtualStates.STATE_COPY_TO_MULTIPLE_STATE_NAME:
                 if virtual_state.key in device.status:
