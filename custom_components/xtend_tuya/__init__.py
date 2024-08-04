fd0c0c42bc43f5791b3198332c00e6d8c3a5be5b
azerty9971 <doyen.gilles@gmail.com>
2024-08-04 16:51:03 +0200

    Fix registry cleanup

--- custom_components/xtend_tuya/__init__.py
+++ custom_components/xtend_tuya/__init__.py
@@ -16,7 +16,7 @@ from tuya_iot.device import (
     PROTOCOL_OTHER,
 )
 
-from homeassistant.config_entries import ConfigEntry
+from homeassistant.config_entries import ConfigEntry, ConfigEntryState
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.exceptions import ConfigEntryAuthFailed, ConfigEntryError, ConfigEntryNotReady
 from homeassistant.helpers import device_registry as dr
@@ -57,6 +57,10 @@ from .tuya_decorators import (
     decorate_tuya_integration
 )
 
+from .util import (
+    get_tuya_integration_runtime_data
+)
+
 # Suppress logs from the library, it logs unneeded on error
 logging.getLogger("tuya_sharing").setLevel(logging.CRITICAL)
 
@@ -89,14 +93,17 @@ async def async_setup_entry(hass: HomeAssistant, entry: XTConfigEntry) -> bool:
     entry.runtime_data = HomeAssistantXTData(multi_manager=multi_manager, reuse_config=multi_manager.reuse_config)
 
     # Cleanup device registry
-    #await cleanup_device_registry(hass, multi_manager)
+    await cleanup_device_registry(hass, multi_manager)
 
     # Register known device IDs
     device_registry = dr.async_get(hass)
-    aggregated_device_map = multi_manager.get_aggregated_device_map()
+    aggregated_device_map = multi_manager.device_map
     for device in aggregated_device_map.values():
         if multi_manager.reuse_config:
-            identifiers = {(DOMAIN_ORIG, device.id), (DOMAIN, device.id)}
+            if device_registry.async_get_device(identifiers={(DOMAIN_ORIG, device.id)}, connections=None):
+                identifiers = {(DOMAIN_ORIG, device.id), (DOMAIN, device.id)}
+            else:
+                identifiers = {(DOMAIN, device.id)}
         else:
             identifiers = {(DOMAIN, device.id)}
         device_registry.async_get_or_create(
@@ -119,13 +126,46 @@ async def async_setup_entry(hass: HomeAssistant, entry: XTConfigEntry) -> bool:
 
 async def cleanup_device_registry(hass: HomeAssistant, multi_manager: MultiManager) -> None:
     """Remove deleted device registry entry if there are no remaining entities."""
+    if not are_all_domain_config_loaded(hass, DOMAIN):
+        return
+    if not are_all_domain_config_loaded(hass, DOMAIN_ORIG):
+        return
     device_registry = dr.async_get(hass)
     for dev_id, device_entry in list(device_registry.devices.items()):
         for item in device_entry.identifiers:
-            if not multi_manager.is_device_in_domain_device_maps([DOMAIN_ORIG, DOMAIN],item):
+            if not is_device_in_domain_device_maps(hass, [DOMAIN_ORIG, DOMAIN],item):
                 device_registry.async_remove_device(dev_id)
                 break
 
+def are_all_domain_config_loaded(hass: HomeAssistant, domain: str) -> bool:
+    config_entries = hass.config_entries.async_entries(domain, False, False)
+    for config_entry in config_entries:
+        if config_entry.state is not ConfigEntryState.LOADED:
+            return False
+    return True
+
+def get_domain_device_map(hass: HomeAssistant, domain: str) -> dict[str, any]:
+    device_map = {}
+    config_entries = hass.config_entries.async_entries(domain, False, False)
+    for config_entry in config_entries:
+        runtime_data = get_tuya_integration_runtime_data(hass, config_entry, domain)
+        for device_id in runtime_data.device_manager.device_map:
+            if device_id not in device_map:
+                device_map[device_id] = runtime_data.device_manager.device_map[device_id]
+    return device_map
+
+def is_device_in_domain_device_maps(hass: HomeAssistant, domains: list[str], device_entry_identifiers: list[str]):
+    device_id = device_entry_identifiers[1]
+    device_domain = device_entry_identifiers[0]
+    if device_domain in domains:
+        for domain in domains:
+            device_map = get_domain_device_map(hass, domain)
+            if device_id in device_map:
+                return True
+    else:
+        return True
+    
+    return False
 
 async def async_unload_entry(hass: HomeAssistant, entry: XTConfigEntry) -> bool:
     #LOGGER.warning(f"async_unload_entry {entry.title} : {entry.data}")
