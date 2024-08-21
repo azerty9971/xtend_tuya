5f0b5f4c7d5f4b6a887aca339c027a01692f08fd
azerty9971 <doyen.gilles@gmail.com>
2024-08-21 14:00:12 +0200

    Update xt_iot_mq.py

--- custom_components/xtend_tuya/multi_manager/tuya_iot/xt_iot_mq.py
+++ custom_components/xtend_tuya/multi_manager/tuya_iot/xt_iot_mq.py
@@ -1,15 +1,16 @@
 from __future__ import annotations
 
-from typing import Any
+from typing import Any, Optional
 
 from tuya_iot import (
     TuyaOpenMQ,
-    TuyaOpenAPI
+    TuyaOpenAPI,
 )
 
 from tuya_iot.openmq import (
     CONNECT_FAILED_NOT_AUTHORISED,
     mqtt,
+    TuyaMQConfig,
 )
 
 from ...const import (
@@ -19,6 +20,11 @@ from ...const import (
 class XTIOTOpenMQ(TuyaOpenMQ):
     def __init__(self, api: TuyaOpenAPI) -> None:
         super().__init__(api)
+
+    def _get_mqtt_config(self) -> Optional[TuyaMQConfig]:
+        config = super()._get_mqtt_config()
+        LOGGER.warning(f"Config => {config}")
+        return config
 
     def _on_connect(self, mqttc: mqtt.Client, user_data: Any, flags, rc):
         if rc == 0:
