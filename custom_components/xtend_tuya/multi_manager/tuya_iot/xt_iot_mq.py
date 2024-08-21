d29f75c6c8f8a88748d01a2af170499afa5a2192
azerty9971 <doyen.gilles@gmail.com>
2024-08-21 14:09:50 +0200

    Update xt_iot_mq.py

--- custom_components/xtend_tuya/multi_manager/tuya_iot/xt_iot_mq.py
+++ custom_components/xtend_tuya/multi_manager/tuya_iot/xt_iot_mq.py
@@ -11,6 +11,10 @@ from tuya_iot.openmq import (
     CONNECT_FAILED_NOT_AUTHORISED,
     mqtt,
     TuyaMQConfig,
+    TO_C_CUSTOM_MQTT_CONFIG_API,
+    AuthType,
+    TO_C_SMART_HOME_MQTT_CONFIG_API,
+    LINK_ID
 )
 
 from ...const import (
@@ -22,9 +26,27 @@ class XTIOTOpenMQ(TuyaOpenMQ):
         super().__init__(api)
 
     def _get_mqtt_config(self) -> Optional[TuyaMQConfig]:
-        config = super()._get_mqtt_config()
-        LOGGER.warning(f"Config => {config}")
-        return config
+        response = self.api.post(
+            TO_C_CUSTOM_MQTT_CONFIG_API
+            if (self.api.auth_type == AuthType.CUSTOM)
+            else TO_C_SMART_HOME_MQTT_CONFIG_API,
+            {
+                "uid": self.api.token_info.uid,
+                "link_id": LINK_ID,
+                "link_type": "mqtt",
+                "topics": "device",
+                "msg_encrypted_version": "2.0"
+                if (self.api.auth_type == AuthType.CUSTOM)
+                else "1.0",
+            },
+        )
+
+        if response.get("success", False) is False:
+            return None
+        
+        LOGGER.warning(f"_get_mqtt_config => {response}")
+
+        return TuyaMQConfig(response)
 
     def _on_connect(self, mqttc: mqtt.Client, user_data: Any, flags, rc):
         if rc == 0:
