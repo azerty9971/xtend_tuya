d3c4bd177927f9a745b3d4662e0b0fa9a8769db3
azerty9971 <doyen.gilles@gmail.com>
2024-08-21 22:13:57 +0200

    Cir dep

---
+++ custom_components/xtend_tuya/multi_manager/shared/device.py
@@ -0,0 +1,71 @@
+from __future__ import annotations
+
+from typing import Any, Optional
+from types import SimpleNamespace
+from dataclasses import dataclass, field
+
+@dataclass
+class XTDeviceStatusRange:
+    code: str
+    type: str
+    values: str
+
+@dataclass
+class XTDeviceFunction:
+    code: str
+    desc: str
+    name: str
+    type: str
+    values: dict[str, Any] = field(default_factory=dict)
+
+class XTDevice(SimpleNamespace):
+    id: str
+    name: str
+    local_key: str
+    category: str
+    product_id: str
+    product_name: str
+    sub: bool
+    uuid: str
+    asset_id: str
+    online: bool
+    icon: str
+    ip: str
+    time_zone: str
+    active_time: int
+    create_time: int
+    update_time: int
+    local_key: str
+    set_up: Optional[bool] = False
+    support_local: Optional[bool] = False
+    local_strategy: dict[int, dict[str, Any]]
+
+    status: dict[str, Any]
+    function: dict[str, XTDeviceFunction]
+    status_range: dict[str, XTDeviceStatusRange]
+
+    force_open_api: Optional[bool] = False
+    data_model: Optional[str] = ""
+
+    def __init__(self, **kwargs: Any) -> None:
+        self.local_strategy = {}
+        self.status = {}
+        self.function = {}
+        self.status_range = {}
+        super().__init__(**kwargs)
+
+    def __eq__(self, other):
+        """If devices are the same one."""
+        return self.id == other.id
+
+    def from_compatible_device(device):
+        return XTDevice(**(device.__dict__))
+    
+    def copy_data_from_device(source_device, dest_device) -> None:
+        if hasattr(source_device, "online") and hasattr(dest_device, "online"):
+            dest_device.online = source_device.online
+        if hasattr(source_device, "name") and hasattr(dest_device, "name"):
+            dest_device.name = source_device.name
+        if hasattr(source_device, "status") and hasattr(dest_device, "status"):
+            for code, value in source_device.status.items():
+                dest_device.status[code] = value
