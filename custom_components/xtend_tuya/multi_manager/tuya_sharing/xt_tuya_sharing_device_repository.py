5f1424d6b37f373e3252ce122c2d25e3e2a2a569
azerty9971 <doyen.gilles@gmail.com>
2024-08-25 12:05:28 +0200

    Split tuya_sharing into multiple files

---
+++ custom_components/xtend_tuya/multi_manager/tuya_sharing/xt_tuya_sharing_device_repository.py
@@ -0,0 +1,108 @@
+from __future__ import annotations
+
+from tuya_sharing.customerapi import (
+    CustomerApi,
+)
+
+from tuya_sharing.device import (
+    CustomerDevice,
+    DeviceRepository,
+    DeviceStatusRange,
+)
+
+from ...const import (
+#    LOGGER,
+    DPType,
+)
+
+from ...base import TuyaEntity
+
+from .xt_tuya_sharing_manager import (
+    XTSharingDeviceManager,
+)
+
+from ..multi_manager import (
+    MultiManager,
+)
+
+class XTSharingDeviceRepository(DeviceRepository):
+    def __init__(self, customer_api: CustomerApi, manager: XTSharingDeviceManager, multi_manager: MultiManager):
+        super().__init__(customer_api)
+        self.manager = manager
+        self.multi_manager = multi_manager
+
+    def update_device_specification(self, device: CustomerDevice):
+        super().update_device_specification(device)
+
+    def _update_device_strategy_info_mod(self, device: CustomerDevice):
+        device_id = device.id
+        response = self.api.get(f"/v1.0/m/life/devices/{device_id}/status")
+        support_local = True
+        if response.get("success"):
+            result = response.get("result", {})
+            pid = result["productKey"]
+            dp_id_map = {}
+            for dp_status_relation in result["dpStatusRelationDTOS"]:
+                if not dp_status_relation["supportLocal"]:
+                    support_local = False
+                    break
+                # statusFormat valueDesc、valueType,enumMappingMap,pid
+                dp_id_map[dp_status_relation["dpId"]] = {
+                    "value_convert": dp_status_relation["valueConvert"],
+                    "status_code": dp_status_relation["statusCode"],
+                    "config_item": {
+                        "statusFormat": dp_status_relation["statusFormat"],
+                        "valueDesc": dp_status_relation["valueDesc"],
+                        "valueType": dp_status_relation["valueType"],
+                        "enumMappingMap": dp_status_relation["enumMappingMap"],
+                        "pid": pid,
+                    }
+                }
+            device.support_local = support_local
+            #if support_local:                      #CHANGED
+            device.local_strategy = dp_id_map       #CHANGED
+
+            #LOGGER.debug(
+            #    f"device status strategy dev_id = {device_id} support_local = {support_local} local_strategy = {dp_id_map}")
+
+    def update_device_strategy_info(self, device: CustomerDevice):
+        #super().update_device_strategy_info(device)
+        self._update_device_strategy_info_mod(device=device)
+        #Sometimes the Type provided by Tuya is ill formed,
+        #replace it with the one from the local strategy
+        for loc_strat in device.local_strategy.values():
+            if "statusCode" not in loc_strat or "valueType" not in loc_strat:
+                continue
+            code = loc_strat["statusCode"]
+            value_type = loc_strat["valueType"]
+
+            if code in device.status_range:
+                device.status_range[code].type = value_type
+            if code in device.function:
+                device.function[code].type     = value_type
+
+            if (
+                "valueDesc"  in loc_strat and
+                code not in device.status_range and
+                code not in device.function
+                ):
+                device.status_range[code] = DeviceStatusRange()
+                device.status_range[code].code   = code
+                device.status_range[code].type   = value_type
+                device.status_range[code].values = loc_strat["valueDesc"]
+
+        #Sometimes the Type provided by Tuya is ill formed,
+        #Try to reformat it into the correct one
+        for status in device.status_range.values():
+            try:
+                DPType(status.type)
+            except ValueError:
+                status.type = TuyaEntity.determine_dptype(status.type)
+        for func in device.function.values():
+            try:
+                DPType(func.type)
+            except ValueError:
+                func.type = TuyaEntity.determine_dptype(func.type)
+
+        self.multi_manager.apply_init_virtual_states(device)
+        self.multi_manager.allow_virtual_devices_not_set_up(device)
--- custom_components/xtend_tuya/multi_manager/tuya_sharing/xt_tuya_sharing_manager.py
+++ custom_components/xtend_tuya/multi_manager/tuya_sharing/xt_tuya_sharing_manager.py
@@ -3,81 +3,27 @@ This file contains all the code that inherit from Tuya integration
 """
 
 from __future__ import annotations
-from typing import Any, NamedTuple
-
-from homeassistant.core import HomeAssistant, callback
+from typing import Any
 
 from tuya_sharing.manager import (
     Manager,
 )
 
-from tuya_sharing.customerapi import (
-    CustomerApi,
-    SharingTokenListener,
-)
 from tuya_sharing.home import (
     SmartLifeHome,
 )
 from tuya_sharing.device import (
     CustomerDevice,
-    DeviceRepository,
-    DeviceStatusRange,
 )
 
 from ...const import (
-    CONF_TOKEN_INFO,
 #    LOGGER,
-    DPType,
     MESSAGE_SOURCE_TUYA_SHARING,
 )
 
 from ..multi_manager import (
     MultiManager,
 )
-
-from .ha_tuya_integration.config_entry_handler import (
-    XTHATuyaIntegrationConfigEntryManager,
-)
-
-from ...base import TuyaEntity
-
-class TuyaSharingData(NamedTuple):
-    device_manager: XTSharingDeviceManager
-    device_ids: list[str] #List of device IDs that are managed by the manager before the managers device merging process
-    ha_tuya_integration_config_manager: XTHATuyaIntegrationConfigEntryManager
-    reuse_config: bool
-
-class XTSharingTokenListener(SharingTokenListener):
-    """Token listener for upstream token updates."""
-
-    def __init__(
-        self,
-        hass: HomeAssistant,
-        entry, #: XTConfigEntry,
-    ) -> None:
-        """Init TokenListener."""
-        self.hass = hass
-        self.entry = entry
-
-    def update_token(self, token_info: dict[str, Any]) -> None:
-        """Update token info in config entry."""
-        data = {
-            **self.entry.data,
-            CONF_TOKEN_INFO: {
-                "t": token_info["t"],
-                "uid": token_info["uid"],
-                "expire_time": token_info["expire_time"],
-                "access_token": token_info["access_token"],
-                "refresh_token": token_info["refresh_token"],
-            },
-        }
-
-        @callback
-        def async_update_entry() -> None:
-            """Update config entry."""
-            self.hass.config_entries.async_update_entry(self.entry, data=data)
-
-        self.hass.add_job(async_update_entry)
 
 class XTSharingDeviceManager(Manager):
     def __init__(
@@ -138,85 +84,3 @@ class XTSharingDeviceManager(Manager):
             other_manager.send_commands(device_id, commands)
             return
         super().send_commands(device_id, commands)
-
-class XTSharingDeviceRepository(DeviceRepository):
-    def __init__(self, customer_api: CustomerApi, manager: XTSharingDeviceManager, multi_manager: MultiManager):
-        super().__init__(customer_api)
-        self.manager = manager
-        self.multi_manager = multi_manager
-
-    def update_device_specification(self, device: CustomerDevice):
-        super().update_device_specification(device)
-
-    def _update_device_strategy_info_mod(self, device: CustomerDevice):
-        device_id = device.id
-        response = self.api.get(f"/v1.0/m/life/devices/{device_id}/status")
-        support_local = True
-        if response.get("success"):
-            result = response.get("result", {})
-            pid = result["productKey"]
-            dp_id_map = {}
-            for dp_status_relation in result["dpStatusRelationDTOS"]:
-                if not dp_status_relation["supportLocal"]:
-                    support_local = False
-                    break
-                # statusFormat valueDesc、valueType,enumMappingMap,pid
-                dp_id_map[dp_status_relation["dpId"]] = {
-                    "value_convert": dp_status_relation["valueConvert"],
-                    "status_code": dp_status_relation["statusCode"],
-                    "config_item": {
-                        "statusFormat": dp_status_relation["statusFormat"],
-                        "valueDesc": dp_status_relation["valueDesc"],
-                        "valueType": dp_status_relation["valueType"],
-                        "enumMappingMap": dp_status_relation["enumMappingMap"],
-                        "pid": pid,
-                    }
-                }
-            device.support_local = support_local
-            #if support_local:                      #CHANGED
-            device.local_strategy = dp_id_map       #CHANGED
-
-            #LOGGER.debug(
-            #    f"device status strategy dev_id = {device_id} support_local = {support_local} local_strategy = {dp_id_map}")
-
-    def update_device_strategy_info(self, device: CustomerDevice):
-        #super().update_device_strategy_info(device)
-        self._update_device_strategy_info_mod(device=device)
-        #Sometimes the Type provided by Tuya is ill formed,
-        #replace it with the one from the local strategy
-        for loc_strat in device.local_strategy.values():
-            if "statusCode" not in loc_strat or "valueType" not in loc_strat:
-                continue
-            code = loc_strat["statusCode"]
-            value_type = loc_strat["valueType"]
-
-            if code in device.status_range:
-                device.status_range[code].type = value_type
-            if code in device.function:
-                device.function[code].type     = value_type
-
-            if (
-                "valueDesc"  in loc_strat and
-                code not in device.status_range and
-                code not in device.function
-                ):
-                device.status_range[code] = DeviceStatusRange()
-                device.status_range[code].code   = code
-                device.status_range[code].type   = value_type
-                device.status_range[code].values = loc_strat["valueDesc"]
-
-        #Sometimes the Type provided by Tuya is ill formed,
-        #Try to reformat it into the correct one
-        for status in device.status_range.values():
-            try:
-                DPType(status.type)
-            except ValueError:
-                status.type = TuyaEntity.determine_dptype(status.type)
-        for func in device.function.values():
-            try:
-                DPType(func.type)
-            except ValueError:
-                func.type = TuyaEntity.determine_dptype(func.type)
-
-        self.multi_manager.apply_init_virtual_states(device)
-        self.multi_manager.allow_virtual_devices_not_set_up(device)
