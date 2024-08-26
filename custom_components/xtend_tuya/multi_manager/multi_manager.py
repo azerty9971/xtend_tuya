630b3d9370a9a36aa1faf3c43aa8c057e6401c07
azerty9971 <doyen.gilles@gmail.com>
2024-08-26 02:21:47 +0200

    Update multi_manager.py

--- custom_components/xtend_tuya/multi_manager/multi_manager.py
+++ custom_components/xtend_tuya/multi_manager/multi_manager.py
@@ -118,7 +118,9 @@ class MultiManager:  # noqa: F811
     async def setup_entry(self, hass: HomeAssistant, config_entry: XTConfigEntry) -> None:
         #Load all the plugins
         subdirs = [x[0] for x in os.walk(os.path.dirname(__file__))]
+        
         LOGGER.warning(f"Found subdirectories: {subdirs}")
+        LOGGER.warning(f"Found subdirectories2: {os.listdir(os.path.dirname(__file__))}")
         for directory in subdirs:
             plugin = importlib.import_module(f"{directory}.init")
             instance: XTDeviceManagerInterface = plugin.get_plugin_instance()
