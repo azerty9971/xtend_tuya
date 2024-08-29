e0aee867fa6f20a844b05e55054c44fc530bbca4
azerty9971 <doyen.gilles@gmail.com>
2024-08-29 05:40:57 +0200

    Fix bug in Tuya IOT library ...

---
+++ custom_components/xtend_tuya/multi_manager/tuya_iot/xt_tuya_iot_openapi.py
@@ -0,0 +1,61 @@
+from __future__ import annotations
+
+from typing import Any
+
+from tuya_iot.openapi import (
+    TuyaOpenAPI,
+    AuthType,
+    TO_C_CUSTOM_REFRESH_TOKEN_API,
+    TO_C_SMART_HOME_REFRESH_TOKEN_API,
+    TUYA_ERROR_CODE_TOKEN_INVALID,
+)
+
+class XTIOTOpenAPI(TuyaOpenAPI):
+    def __init__(
+        self,
+        endpoint: str,
+        access_id: str,
+        access_secret: str,
+        auth_type: AuthType = AuthType.SMART_HOME,
+        lang: str = "en",
+    ) -> None:
+        super().__init__(endpoint=endpoint, access_id=access_id, access_secret=access_secret, auth_type=auth_type, lang=lang)
+        self.connect_response = None
+    
+    def connect(
+        self,
+        username: str = "",
+        password: str = "",
+        country_code: str = "",
+        schema: str = "",
+    ) -> dict[str, Any]:
+        self.connect_response = super().connect(username=username, password=password, country_code=country_code, schema=schema)
+        return self.connect_response
+    
+    def get(self, path: str, params: dict[str, Any] | None = None) -> dict[str, Any]:
+        normal_result = super().get(path=path, params=params)
+        if ( 
+            normal_result.get("code", -1) == TUYA_ERROR_CODE_TOKEN_INVALID and
+            self.connect_response is not None and
+            (
+                path.startswith(TO_C_CUSTOM_REFRESH_TOKEN_API) or
+                path.startswith(TO_C_SMART_HOME_REFRESH_TOKEN_API)
+            )
+        ):
+            normal_result = self.connect_response
+            self.connect_response = None
+        return normal_result
+
+    def post(self, path: str, body: dict[str, Any] | None = None) -> dict[str, Any]:
+        normal_result = super().post(path=path, body=body)
+        if ( 
+            normal_result.get("code", -1) == TUYA_ERROR_CODE_TOKEN_INVALID and
+            self.connect_response is not None and
+            (
+                path.startswith(TO_C_CUSTOM_REFRESH_TOKEN_API) or
+                path.startswith(TO_C_SMART_HOME_REFRESH_TOKEN_API)
+            )
+        ):
+            normal_result = self.connect_response
+            self.connect_response = None
+        return normal_result
