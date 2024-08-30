ef36678c574a90e567f4cabc4d623e928cd74b24
azerty9971 <doyen.gilles@gmail.com>
2024-08-30 04:03:36 +0200

    Remove aliases from status

--- custom_components/xtend_tuya/multi_manager/shared/cloud_fix.py
+++ custom_components/xtend_tuya/multi_manager/shared/cloud_fix.py
@@ -7,6 +7,7 @@ from .device import (
 class CloudFixes:
     def apply_fixes(device: XTDevice):
         CloudFixes._fix_missing_local_strategy_enum_mapping_map(device)
+        CloudFixes._remove_status_that_are_local_strategy_aliases(device)
 
     def _fix_missing_local_strategy_enum_mapping_map(device: XTDevice):
         for local_strategy in device.local_strategy.values():
@@ -16,3 +17,10 @@ class CloudFixes:
                         mappings[str(False)] = mappings['false']
                     if 'true' in mappings and str(True) not in mappings:
                         mappings[str(True)] = mappings['true']
+    
+    def _remove_status_that_are_local_strategy_aliases(device: XTDevice):
+        for local_strategy in device.local_strategy.values():
+            if aliases := local_strategy.get("status_code_alias", None):
+                for alias in aliases:
+                    if alias in device.status:
+                        device.status.pop(alias)
