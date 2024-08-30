440c07575908c382a81a484d9b61424747290efd
azerty9971 <doyen.gilles@gmail.com>
2024-08-30 04:42:48 +0200

    Update cloud_fix.py

--- custom_components/xtend_tuya/multi_manager/shared/cloud_fix.py
+++ custom_components/xtend_tuya/multi_manager/shared/cloud_fix.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import json
+
 from .device import (
     XTDevice,
 )
@@ -7,6 +9,7 @@ from .device import (
 class CloudFixes:
     def apply_fixes(device: XTDevice):
         CloudFixes._fix_missing_local_strategy_enum_mapping_map(device)
+        CloudFixes._fix_missing_aliases_using_status_format(device)
         CloudFixes._remove_status_that_are_local_strategy_aliases(device)
 
     def _fix_missing_local_strategy_enum_mapping_map(device: XTDevice):
@@ -17,6 +20,16 @@ class CloudFixes:
                         mappings[str(False)] = mappings['false']
                     if 'true' in mappings and str(True) not in mappings:
                         mappings[str(True)] = mappings['true']
+    
+    def _fix_missing_aliases_using_status_format(device: XTDevice):
+        for local_strategy in device.local_strategy.values():
+            status_code = local_strategy.get("status_code", None)
+            if config_item := local_strategy.get("config_item", None):
+                if status_formats := config_item.get("statusFormat", None):
+                    status_formats_dict = json.loads(status_formats)
+                    for status in status_formats_dict:
+                        if status != status_code and status not in local_strategy["status_code_alias"]:
+                            local_strategy["status_code_alias"].append(status)
     
     def _remove_status_that_are_local_strategy_aliases(device: XTDevice):
         for local_strategy in device.local_strategy.values():
