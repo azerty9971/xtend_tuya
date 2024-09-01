b8882666d78962fdb80900196928ff72df6b6257
azerty9971 <doyen.gilles@gmail.com>
2024-09-01 20:56:05 +0200

    Debug

--- custom_components/xtend_tuya/multi_manager/tuya_sharing/xt_tuya_sharing_manager.py
+++ custom_components/xtend_tuya/multi_manager/tuya_sharing/xt_tuya_sharing_manager.py
@@ -18,6 +18,7 @@ from tuya_sharing.home import (
 from tuya_sharing.device import (
     CustomerDevice,
 )
+from tuya_sharing.strategy import strategy
 
 from .import_stub import (
     XTSharingDeviceManager,
@@ -90,6 +91,32 @@ class XTSharingDeviceManager(Manager):  # noqa: F811
         status_new = self.multi_manager.convert_device_report_status_list(device_id, status)
         status_new = self.multi_manager.multi_source_handler.filter_status_list(device_id, MESSAGE_SOURCE_TUYA_SHARING, status_new)
         status_new = self.multi_manager.virtual_state_handler.apply_virtual_states_to_status_list(device, status_new)
+
+        #DEBUG
+        device = self.device_map.get(device_id, None)
+        if not device:
+            return
+        if self.multi_manager.device_watcher.is_watched(device_id):
+            LOGGER.warning(f"mq _on_device_report-> {status}")
+        if device.support_local:
+            for item in status:
+                if "dpId" in item and "value" in item:
+                    dp_id_item = device.local_strategy[item["dpId"]]
+                    strategy_name = dp_id_item["value_convert"]
+                    config_item = dp_id_item["config_item"]
+                    dp_item = (dp_id_item["status_code"], item["value"])
+                    if self.multi_manager.device_watcher.is_watched(device_id):
+                        LOGGER.warning(
+                            f"mq _on_device_report before strategy convert strategy_name={strategy_name},dp_item={dp_item},config_item={config_item}")
+                    code, value = strategy.convert(strategy_name, dp_item, config_item)
+                    if self.multi_manager.device_watcher.is_watched(device_id):
+                        LOGGER.warning(f"mq _on_device_report after strategy convert code={code},value={value}")
+        else:
+            for item in status:
+                if "code" in item and "value" in item:
+                    code = item["code"]
+                    value = item["value"]
+        #ENDDEBUG
         super()._on_device_report(device_id, status_new)
     
     def send_commands(
