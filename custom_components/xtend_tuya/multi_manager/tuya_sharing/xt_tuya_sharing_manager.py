0c732dd267824d45e27a4c30e5f6b19c782e53ce
azerty9971 <doyen.gilles@gmail.com>
2024-09-03 12:57:12 +0200

    Change position of device registration

--- custom_components/xtend_tuya/multi_manager/tuya_sharing/xt_tuya_sharing_manager.py
+++ custom_components/xtend_tuya/multi_manager/tuya_sharing/xt_tuya_sharing_manager.py
@@ -74,6 +74,13 @@ class XTSharingDeviceManager(Manager):  # noqa: F811
         if self.other_device_manager is not None:
             if self.mq and self.mq != self.other_device_manager.mq:
                 self.mq.stop()
+                #Copy devices to Tuya's device_map so that they register in the MQTT queue
+                for device in self.other_device_manager.device_map.values():
+                    if device.id not in self.other_device_manager.device_map:
+                        self.multi_manager.device_watcher.report_message(device.id, "Added to Tuya's device_map")
+                        self.other_device_manager.device_map[device.id] = device
+                    else:
+                        self.multi_manager.device_watcher.report_message(device.id, "Already in Tuya's device_map")
             self.other_device_manager.refresh_mq()
             for device in self.other_device_manager.mq.device:
                 self.multi_manager.device_watcher.report_message(device.id, "Registered in MQTT")
