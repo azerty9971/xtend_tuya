9ff5641f1c185397194b4f1e87ae063b8f73085d
azerty9971 <doyen.gilles@gmail.com>
2024-09-04 19:12:13 +0200

    Debug G30

--- custom_components/xtend_tuya/multi_manager/multi_manager.py
+++ custom_components/xtend_tuya/multi_manager/multi_manager.py
@@ -283,12 +283,12 @@ class MultiManager:  # noqa: F811
         if not dev_id:
             LOGGER.warning(f"dev_id {dev_id} not found!")
             return
-        self.device_watcher.report_message(dev_id, f"On Message ({source}): {msg}")
         
         #self.device_watcher.report_message(dev_id, f"on_message ({source}) => {msg}")
 
         new_message = self._convert_message_for_all_accounts(msg)
         if status_list := self._get_status_list_from_message(msg):
+            self.device_watcher.report_message(dev_id, f"On Message reporting ({source}): {msg}")
             self.multi_source_handler.register_status_list_from_source(dev_id, source, status_list)
             #self.device_watcher.report_message(dev_id, f"on_message ({source}) status list => {status_list}")
         
