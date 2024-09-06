da46165a4a040cc10d30c336cf08b0a3ffc6dead
azerty9971 <doyen.gilles@gmail.com>
2024-09-06 21:07:26 +0200

    Revert "Update shared_classes.py"
    This reverts commit c75288e735635e3e1d15940d9adc7dd9ede8385f.

--- custom_components/xtend_tuya/multi_manager/shared/shared_classes.py
+++ custom_components/xtend_tuya/multi_manager/shared/shared_classes.py
@@ -22,7 +22,7 @@ from ...const import (
 
 class DeviceWatcher:
     def __init__(self, multi_manager: MultiManager) -> None:
-        self.watched_dev_id = ["bf6e91majilvk7mv"]
+        self.watched_dev_id = []
         self.multi_manager = multi_manager
 
     def is_watched(self, dev_id: str) -> bool:
