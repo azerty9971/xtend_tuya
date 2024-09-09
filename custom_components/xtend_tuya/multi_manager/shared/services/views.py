5857392842c1c6d6c6fde8f86eb89a1e4fe75fb1
azerty9971 <doyen.gilles@gmail.com>
2024-09-10 01:35:47 +0200

    Update views.py

--- custom_components/xtend_tuya/multi_manager/shared/services/views.py
+++ custom_components/xtend_tuya/multi_manager/shared/services/views.py
@@ -19,7 +19,7 @@ class XTEntityView(HomeAssistantView):
         """Initialize a basic camera view."""
         self.component = component
         self.name = "api:" + DOMAIN + ":" + name
-        self.url = "/api/" + DOMAIN + name + "/{entity_id}"
+        self.url = "/api/" + DOMAIN + "/" + name + "/{entity_id}"
         self.requires_auth = requires_auth
 
 
@@ -53,7 +53,7 @@ class XTGeneralView(HomeAssistantView):
     def __init__(self, name: str, callback, requires_auth: bool = True) -> None:
         """Initialize a basic camera view."""
         self.name = "api:" + DOMAIN + ":" + name
-        self.url = "/api/" + DOMAIN + name
+        self.url = "/api/" + DOMAIN + "/" + name
         self.requires_auth = requires_auth
         self.callback = callback
 
