81fa0e8d6891044402347a0fcdafb710f6ddea52
azerty9971 <doyen.gilles@gmail.com>
2024-09-10 23:03:28 +0200

    Update xt_tuya_iot_manager.py

--- custom_components/xtend_tuya/multi_manager/tuya_iot/xt_tuya_iot_manager.py
+++ custom_components/xtend_tuya/multi_manager/tuya_iot/xt_tuya_iot_manager.py
@@ -6,6 +6,7 @@ https://github.com/tuya/tuya-iot-python-sdk
 from __future__ import annotations
 import json
 import copy
+import time
 from tuya_iot import (
     TuyaDeviceManager,
     TuyaOpenAPI,
@@ -37,10 +38,15 @@ from .xt_tuya_iot_mq import (
 from .xt_tuya_iot_ipc_listener import (
     XTIOTIPCListener,
 )
+from paho.mqtt.enums import (
+    MQTTErrorCode,
+)
 
 
 class XTIOTDeviceManager(TuyaDeviceManager):
-    def __init__(self, multi_manager: MultiManager, api: TuyaOpenAPI, mq: TuyaOpenMQ) -> None:
+    def __init__(
+        self, multi_manager: MultiManager, api: TuyaOpenAPI, mq: TuyaOpenMQ
+    ) -> None:
         super().__init__(api, mq)
         mq.remove_message_listener(self.on_message)
         mq.add_message_listener(self.forward_message_to_multi_manager)
@@ -48,9 +54,11 @@ class XTIOTDeviceManager(TuyaDeviceManager):
         self.ipc_mq: XTIOTOpenMQIPC = XTIOTOpenMQIPC(api)
         self.ipc_listener: XTIOTIPCListener = XTIOTIPCListener(self.multi_manager)
         self.ipc_mq.start()
-        self.ipc_mq.add_message_listener(self.ipc_listener.forward_message_to_multi_manager)
+        self.ipc_mq.add_message_listener(
+            self.ipc_listener.forward_message_to_multi_manager
+        )
 
-    def forward_message_to_multi_manager(self, msg:str):
+    def forward_message_to_multi_manager(self, msg: str):
         self.multi_manager.on_message(MESSAGE_SOURCE_TUYA_IOT, msg)
 
     def get_device_info(self, device_id: str) -> dict[str, Any]:
@@ -68,7 +76,7 @@ class XTIOTDeviceManager(TuyaDeviceManager):
                 result = response["result"]
                 result["online"] = result["is_online"]
                 return response
-    
+
     def get_device_status(self, device_id: str) -> dict[str, Any]:
         """Get device status.
 
@@ -86,12 +94,12 @@ class XTIOTDeviceManager(TuyaDeviceManager):
             if response["success"]:
                 return response
 
-    #Copy of the Tuya original method with some minor modifications
+    # Copy of the Tuya original method with some minor modifications
     def update_device_list_in_smart_home_mod(self):
         response = self.api.get(f"/v1.0/users/{self.api.token_info.uid}/devices")
         if response["success"]:
             for item in response["result"]:
-                device = XTDevice(**item)       #CHANGED
+                device = XTDevice(**item)  # CHANGED
                 status = {}
                 for item_status in device.status:
                     if "code" in item_status and "value" in item_status:
@@ -101,14 +109,16 @@ class XTIOTDeviceManager(TuyaDeviceManager):
                 device.status = status
                 self.device_map[item["id"]] = device
 
-        #ADDED
+        # ADDED
         self.update_device_list_in_smart_home_from_sharing()
-        #END ADDED
+        # END ADDED
 
         self.update_device_function_cache()
 
     def update_device_list_in_smart_home_from_sharing(self):
-        response = self.api.get(f"/v1.0/users/{self.api.token_info.uid}/devices?from=sharing")
+        response = self.api.get(
+            f"/v1.0/users/{self.api.token_info.uid}/devices?from=sharing"
+        )
         if response["success"]:
             for item in response["result"]:
                 device = XTDevice(**item)
@@ -123,7 +133,7 @@ class XTIOTDeviceManager(TuyaDeviceManager):
 
     def update_device_list_in_smart_home(self):
         self.update_device_list_in_smart_home_mod()
-    
+
     def update_device_function_cache(self, devIds: list = []):
         super().update_device_function_cache(devIds)
         for device_id in self.device_map:
@@ -134,7 +144,7 @@ class XTIOTDeviceManager(TuyaDeviceManager):
 
     def on_message(self, msg: str):
         super().on_message(msg)
-    
+
     def _on_device_other(self, device_id: str, biz_code: str, data: dict[str, Any]):
         return super()._on_device_other(device_id, biz_code, data)
 
@@ -142,9 +152,15 @@ class XTIOTDeviceManager(TuyaDeviceManager):
         device = self.device_map.get(device_id, None)
         if not device:
             return
-        status_new = self.multi_manager.convert_device_report_status_list(device_id, status)
-        status_new = self.multi_manager.multi_source_handler.filter_status_list(device_id, MESSAGE_SOURCE_TUYA_IOT, status_new)
-        status_new = self.multi_manager.virtual_state_handler.apply_virtual_states_to_status_list(device, status_new)
+        status_new = self.multi_manager.convert_device_report_status_list(
+            device_id, status
+        )
+        status_new = self.multi_manager.multi_source_handler.filter_status_list(
+            device_id, MESSAGE_SOURCE_TUYA_IOT, status_new
+        )
+        status_new = self.multi_manager.virtual_state_handler.apply_virtual_states_to_status_list(
+            device, status_new
+        )
         super()._on_device_report(device_id, status_new)
 
     def _update_device_list_info_cache(self, devIds: list[str]):
@@ -153,13 +169,13 @@ class XTIOTDeviceManager(TuyaDeviceManager):
         for item in result.get("list", []):
             device_id = item["id"]
             self.device_map[device_id] = XTDevice(**item)
-    
+
     def get_device_properties(self, device: XTDevice) -> XTDeviceProperties | None:
         device_properties = XTDeviceProperties()
         device_properties.function = copy.deepcopy(device.function)
         device_properties.status_range = copy.deepcopy(device.status_range)
         device_properties.status = copy.deepcopy(device.status)
-        if (hasattr(device, "local_strategy")):
+        if hasattr(device, "local_strategy"):
             device_properties.local_strategy = copy.deepcopy(device.local_strategy)
         response = self.api.get(f"/v2.0/cloud/thing/{device.id}/shadow/properties")
         response2 = self.api.get(f"/v2.0/cloud/thing/{device.id}/model")
@@ -167,26 +183,30 @@ class XTIOTDeviceManager(TuyaDeviceManager):
             LOGGER.warning(f"Response1: {response}")
             LOGGER.warning(f"Response1: {response2}")
             return
-        
+
         if response2.get("success"):
             result = response2.get("result", {})
             data_model = json.loads(result.get("model", "{}"))
             device_properties.data_model = data_model
             for service in data_model["services"]:
                 for property in service["properties"]:
-                    if (    "abilityId" in property
+                    if (
+                        "abilityId" in property
                         and "code" in property
                         and "accessMode" in property
                         and "typeSpec" in property
-                        ):
+                    ):
                         dp_id = int(property["abilityId"])
-                        code  = property["code"]
+                        code = property["code"]
                         typeSpec = property["typeSpec"]
                         real_type = TuyaEntity.determine_dptype(typeSpec["type"])
                         typeSpec.pop("type")
                         typeSpec_json = json.dumps(typeSpec)
                         if dp_id not in device_properties.local_strategy:
-                            if code in device_properties.function or code in device_properties.status_range:
+                            if (
+                                code in device_properties.function
+                                or code in device_properties.status_range
+                            ):
                                 property_update = False
                             else:
                                 property_update = True
@@ -201,7 +221,7 @@ class XTIOTDeviceManager(TuyaDeviceManager):
                                 },
                                 "property_update": property_update,
                                 "use_open_api": True,
-                                "status_code_alias": []
+                                "status_code_alias": [],
                             }
 
         if response.get("success"):
@@ -212,9 +232,12 @@ class XTIOTDeviceManager(TuyaDeviceManager):
                 dp_id = int(dp_property["dp_id"])
                 if "dp_id" in dp_property and "type" in dp_property:
                     code = dp_property["code"]
-                    dp_type = dp_property.get("type",None)
+                    dp_type = dp_property.get("type", None)
                     if dp_id not in device_properties.local_strategy:
-                        if code in device_properties.function or code in device_properties.status_range:
+                        if (
+                            code in device_properties.function
+                            or code in device_properties.status_range
+                        ):
                             property_update = False
                         else:
                             property_update = True
@@ -230,40 +253,50 @@ class XTIOTDeviceManager(TuyaDeviceManager):
                             },
                             "property_update": property_update,
                             "use_open_api": True,
-                            "status_code_alias": []
+                            "status_code_alias": [],
                         }
-                if (    "code"  in dp_property 
-                    and "dp_id" in dp_property 
+                if (
+                    "code" in dp_property
+                    and "dp_id" in dp_property
                     and dp_id in device_properties.local_strategy
-                    ):
+                ):
                     code = dp_property["code"]
-                    if code not in device_properties.status_range and code not in device_properties.function :
-                        device_properties.status_range[code] = XTDeviceStatusRange(code=code, 
-                                                                                   type=device_properties.local_strategy[dp_id]["config_item"]["valueType"],
-                                                                                   values=device_properties.local_strategy[dp_id]["config_item"]["valueDesc"])
+                    if (
+                        code not in device_properties.status_range
+                        and code not in device_properties.function
+                    ):
+                        device_properties.status_range[code] = XTDeviceStatusRange(
+                            code=code,
+                            type=device_properties.local_strategy[dp_id]["config_item"][
+                                "valueType"
+                            ],
+                            values=device_properties.local_strategy[dp_id][
+                                "config_item"
+                            ]["valueDesc"],
+                        )
                     if code not in device_properties.status:
-                        device_properties.status[code] = dp_property.get("value",None)
+                        device_properties.status[code] = dp_property.get("value", None)
         return device_properties
 
-    def send_property_update(
-            self, device_id: str, properties: list[dict[str, Any]]
-    ):
+    def send_property_update(self, device_id: str, properties: list[dict[str, Any]]):
         for property in properties:
             for prop_key in property:
-                property_str = f"{{\"{prop_key}\":{property[prop_key]}}}"
-                self.api.post(f"/v2.0/cloud/thing/{device_id}/shadow/properties/issue", {"properties": property_str}
-        )
-    
-    def send_lock_unlock_command(
-            self, device_id: str, lock: bool
-    ) -> bool:
+                property_str = f'{{"{prop_key}":{property[prop_key]}}}'
+                self.api.post(
+                    f"/v2.0/cloud/thing/{device_id}/shadow/properties/issue",
+                    {"properties": property_str},
+                )
+
+    def send_lock_unlock_command(self, device_id: str, lock: bool) -> bool:
         supported_unlock_types: list[str] = []
         if lock:
             open = "false"
         else:
             open = "true"
 
-        remote_unlock_types = self.api.get(f"/v1.0/devices/{device_id}/door-lock/remote-unlocks")
+        remote_unlock_types = self.api.get(
+            f"/v1.0/devices/{device_id}/door-lock/remote-unlocks"
+        )
         if remote_unlock_types.get("success", False):
             results = remote_unlock_types.get("result", [])
             for result in results:
@@ -271,34 +304,57 @@ class XTIOTDeviceManager(TuyaDeviceManager):
                     if supported_unlock_type := result.get("remote_unlock_type", None):
                         supported_unlock_types.append(supported_unlock_type)
         if "remoteUnlockWithoutPwd" in supported_unlock_types:
-            ticket = self.api.post(f"/v1.0/devices/{device_id}/door-lock/password-ticket")
+            ticket = self.api.post(
+                f"/v1.0/devices/{device_id}/door-lock/password-ticket"
+            )
             if ticket.get("success", False):
                 result = ticket.get("result", {})
                 if ticket_id := result.get("ticket_id", None):
-                    lock_operation = self.api.post(f"/v1.0/smart-lock/devices/{device_id}/password-free/door-operate", {"ticket_id": ticket_id, "open": open})
+                    lock_operation = self.api.post(
+                        f"/v1.0/smart-lock/devices/{device_id}/password-free/door-operate",
+                        {"ticket_id": ticket_id, "open": open},
+                    )
                     return lock_operation.get("success", False)
         return False
-    
+
     def get_sdp_answer(self, device_id: str, sdp_offer: str) -> str | None:
         LOGGER.warning(f"get_sdp_answer for {device_id}")
         if webrtc_config := self._get_webrtc_config(device_id):
             auth_token = webrtc_config.get("auth")
-            moto_id =  webrtc_config.get("moto_id")
+            moto_id = webrtc_config.get("moto_id")
             for topic in self.ipc_mq.mq_config.sink_topic.values():
                 topic = topic.replace("{device_id}", device_id)
                 topic = topic.replace("{moto_id}", moto_id)
                 LOGGER.warning(f"Computed topic: {topic}")
-                subscribe_result = self.mq.client.subscribe(topic)
-                LOGGER.warning(f"Susbscribe result: {subscribe_result}")
+                result, mid = self.mq.client.subscribe(topic)
+                if result == MQTTErrorCode.MQTT_ERR_SUCCESS:
+                    payload = {
+                        "protocol":302,
+                        "pv":"2.2",
+                        "t":time.time_ns() // 1_000_000,
+                        "data":{
+                            "from":"TEST_FROM_HA",
+                            "to":f"{device_id}",
+                            "session_id":"262626",
+                            "moto_id":f"{moto_id}",
+                            "type":"offer"
+                        },
+                        "msg":{
+                            "sdp":f"{sdp_offer}",
+                            "auth":f"{auth_token}",
+                            "mode":"webrtc",
+                            "stream_type":1
+                        }
+                    }
+                    self._publish_to_ipc_mqtt(topic, json.dumps(payload))
 
-            
             if not auth_token or not moto_id:
                 return None
-            
+
         return None
 
     def _publish_to_ipc_mqtt(self, topic: str, msg: str):
-        self.ipc_mq.client.publish(topic=topic, payload=msg)
+        LOGGER.warning(f"Result of MQTT publish: {self.ipc_mq.client.publish(topic=topic, payload=msg)}")
 
     def _get_webrtc_config(self, device_id: str):
         webrtc_config = self.api.get(f"/v1.0/devices/{device_id}/webrtc-configs")
