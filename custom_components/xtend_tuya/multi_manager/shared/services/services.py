24ec4e9961f4af8731725348b4ba0dd4a90c233e
azerty9971 <doyen.gilles@gmail.com>
2024-09-15 19:26:03 +0200

    Prepare auth

--- custom_components/xtend_tuya/multi_manager/shared/services/services.py
+++ custom_components/xtend_tuya/multi_manager/shared/services/services.py
@@ -76,7 +76,7 @@ class ServiceManager:
         self._register_service(DOMAIN, SERVICE_GET_CAMERA_STREAM_URL, self._handle_get_camera_stream_url, SERVICE_GET_CAMERA_STREAM_URL_SCHEMA, True, True, True)
         self._register_service(DOMAIN, SERVICE_CALL_API, self._handle_call_api, SERVICE_CALL_API_SCHEMA, True, True, True)
         self._register_service(DOMAIN, SERVICE_GET_ICE_SERVERS, self._handle_get_ice_servers, SERVICE_GET_ICE_SERVERS_SCHEMA, True, True, False)
-        self._register_service(DOMAIN, SERVICE_WEBRTC, self._handle_webrtc, SERVICE_GET_CAMERA_STREAM_URL_SCHEMA, False, True, False)
+        self._register_service(DOMAIN, SERVICE_WEBRTC, self._handle_webrtc, SERVICE_GET_CAMERA_STREAM_URL_SCHEMA, True, True, False)
 
     def _register_service(self, domain: str, name: str, callback, schema, requires_auth: bool = True, allow_from_api:bool = True, use_cache:bool = True):
         self.hass.services.async_register(
