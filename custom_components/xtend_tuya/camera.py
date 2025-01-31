0ed9a8b3792ea7e4724b99772921900a82edfa66
azerty9971 <doyen.gilles@gmail.com>
2025-01-31 17:14:17 +0100

    Update camera.py

--- custom_components/xtend_tuya/camera.py
+++ custom_components/xtend_tuya/camera.py
@@ -56,10 +56,10 @@ async def async_setup_entry(
 
         async_add_entities(entities)
 
-    await async_discover_device([*hass_data.manager.device_map])
+    async_discover_device([*hass_data.manager.device_map])
 
     entry.async_on_unload(
-        async_dispatcher_connect(hass, TUYA_DISCOVERY_NEW, await async_discover_device)
+        async_dispatcher_connect(hass, TUYA_DISCOVERY_NEW, async_discover_device)
     )
 
 
