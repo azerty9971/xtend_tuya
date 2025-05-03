932ca2fbd1f4403c4b88f1d6c28a66ac329a6bed
azerty9971 <doyen.gilles@gmail.com>
2025-05-03 20:46:09 +0200

    Update xt_tuya_iot_mq.py

--- custom_components/xtend_tuya/multi_manager/tuya_iot/xt_tuya_iot_mq.py
+++ custom_components/xtend_tuya/multi_manager/tuya_iot/xt_tuya_iot_mq.py
@@ -27,6 +27,8 @@ from tuya_iot.openmq import (
     TO_C_CUSTOM_MQTT_CONFIG_API,
     AuthType,
     TO_C_SMART_HOME_MQTT_CONFIG_API,
+    CONNECT_FAILED_NOT_AUTHORISED,
+    time,
 )
 
 from ...const import (
@@ -52,6 +54,14 @@ class XTIOTOpenMQ(TuyaOpenMQ):
     def __init__(self, api: TuyaOpenAPI) -> None:
         super().__init__(api)
         self.api: XTIOTOpenAPI = api
+
+    def run(self):
+        """Method representing the thread's activity which should not be used directly."""
+        while not self._stop_event.is_set():
+            self.__run_mqtt()
+
+            # reconnect every 2 hours required.
+            time.sleep(self.mq_config.expire_time - 60)
 
     def _get_mqtt_config(self) -> Optional[XTIOTTuyaMQConfig]:
         if not self.api.is_connect():
@@ -90,6 +100,12 @@ class XTIOTOpenMQ(TuyaOpenMQ):
     # def _on_publish(self, mqttc: mqtt.Client, user_data: Any, mid: int, reason_code: mqtt_ReasonCode = None, properties: mqtt_Properties = None):
     #     pass
 
+    def _on_disconnect(self, client, userdata, rc):
+        if rc != 0:
+            LOGGER.error(f"Unexpected disconnection.{rc}")
+        else:
+            LOGGER.debug("disconnect")
+
     def _start(self, mq_config: TuyaMQConfig) -> mqtt.Client:
         #mqttc = mqtt.Client(callback_api_version=mqtt_CallbackAPIVersion.VERSION2 ,client_id=mq_config.client_id)
         mqttc = mqtt.Client(client_id=mq_config.client_id)
@@ -110,3 +126,26 @@ class XTIOTOpenMQ(TuyaOpenMQ):
 
         mqttc.loop_start()
         return mqttc
+    
+    def _on_connect(self, mqttc: mqtt.Client, user_data: Any, flags, rc):
+        LOGGER.debug(f"connect flags->{flags}, rc->{rc}")
+        if rc == 0:
+            for (key, value) in self.mq_config.source_topic.items():
+                mqttc.subscribe(value)
+        elif rc == CONNECT_FAILED_NOT_AUTHORISED:
+            self.__run_mqtt()
+
+    def __run_mqtt(self):
+        mq_config = self._get_mqtt_config()
+        if mq_config is None:
+            LOGGER.error("error while get mqtt config")
+            return
+
+        self.mq_config = mq_config
+
+        LOGGER.debug(f"connecting {mq_config.url}")
+        mqttc = self._start(mq_config)
+
+        if self.client:
+            self.client.disconnect()
+        self.client = mqttc
