eefbaa9c4f5955568996975067cbbdb7dea43b61
azerty9971 <doyen.gilles@gmail.com>
2025-06-13 06:53:51 +0200

    Update camera.py

--- custom_components/xtend_tuya/camera.py
+++ custom_components/xtend_tuya/camera.py
@@ -198,7 +198,7 @@ class XTCameraEntity(XTEntity, TuyaCameraEntity):
         return await self.iot_manager.async_handle_async_webrtc_offer(offer_sdp, session_id, send_message, self.device, self.hass)
     
     @callback
-    def async_close_session(self, session_id: str) -> None:
+    def close_webrtc_session(self, session_id: str) -> None:
         """Close the session."""
         LOGGER.warning(f"Closing WebRTC session {session_id}")
         return  ## This is an optional method so we need a default here.
