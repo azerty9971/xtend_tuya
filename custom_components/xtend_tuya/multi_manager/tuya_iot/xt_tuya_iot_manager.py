d1d99a6ea4293a4072d612922ea5847d8dcf30f9
azerty9971 <doyen.gilles@gmail.com>
2025-07-16 20:40:58 +0200

    Update xt_tuya_iot_manager.py

--- custom_components/xtend_tuya/multi_manager/tuya_iot/xt_tuya_iot_manager.py
+++ custom_components/xtend_tuya/multi_manager/tuya_iot/xt_tuya_iot_manager.py
@@ -349,7 +349,7 @@ class XTIOTDeviceManager(TuyaDeviceManager):
         if manual_unlock_code := cast(list[XTDPCode], device.get_preference(XTDevice.XTDevicePreference.LOCK_MANUAL_UNLOCK_COMMAND)):
             commands: list[dict[str, Any]] = []
             for dpcode in manual_unlock_code:
-                commands.append({"code": dpcode, "value": open})
+                commands.append({"code": dpcode, "value": not lock})
             self.multi_manager.send_commands(device_id=device.id, commands=commands)
             return True #Assume that the command worked...
         return False
