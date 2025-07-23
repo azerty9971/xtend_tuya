244f3ede70a087c307b9b91779c87e4985aef668
azerty9971 <doyen.gilles@gmail.com>
2025-07-23 12:27:13 +0200

    Update shared_classes.py

--- custom_components/xtend_tuya/multi_manager/shared/shared_classes.py
+++ custom_components/xtend_tuya/multi_manager/shared/shared_classes.py
@@ -22,7 +22,7 @@ from ...const import (
 
 class DeviceWatcher:
     def __init__(self, multi_manager: mm.MultiManager) -> None:
-        self.watched_dev_id: list[str] = []
+        self.watched_dev_id: list[str] = ["bfd2bc6b252907fc0dnlao"]
         self.multi_manager = multi_manager
 
     def is_watched(self, dev_id: str) -> bool:
