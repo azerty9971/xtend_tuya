a1c5aca7ea99403c8612a1d83a2b56d7b26773b7
azerty9971 <doyen.gilles@gmail.com>
2025-08-09 21:33:45 +0200

    Update switch.py

--- custom_components/xtend_tuya/switch.py
+++ custom_components/xtend_tuya/switch.py
@@ -414,6 +414,8 @@ async def async_setup_entry(
         device_ids = [*device_map]
         for device_id in device_ids:
             if device := hass_data.manager.device_map.get(device_id):
+                if restrict_dpcode is not None:
+                    LOGGER.warning(f"Calling with restriction: {restrict_dpcode} => {supported_descriptors.get(device.category)} => {XTEntityDescriptorManager.get_category_keys(supported_descriptors.get(device.category))}")
                 if category_descriptions := supported_descriptors.get(device.category):
                     externally_managed_dpcodes = (
                         XTEntityDescriptorManager.get_category_keys(
