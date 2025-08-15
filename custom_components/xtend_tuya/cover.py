aed60036ef1096cc47065126032f8acbdf5f884d
azerty9971 <doyen.gilles@gmail.com>
2025-08-15 18:15:17 +0200

    Update cover.py

--- custom_components/xtend_tuya/cover.py
+++ custom_components/xtend_tuya/cover.py
@@ -222,7 +222,7 @@ class XTCoverEntity(XTEntity, TuyaCoverEntity):
         self.device = device
         self.local_hass = hass
         device_manager.post_setup_callbacks.append(self.add_cover_open_close_option)
-        LOGGER.warning(f"Added {device.name} => current_position: {self._current_position} <=> supported_features: {self._attr_supported_features}")
+        LOGGER.warning(f"Added {device.name} => current_position: {self._current_position} <=> state: {self.state}")
 
     @property
     def is_cover_control_inverted(self) -> bool | None:
