bfeea2e67101ba7b1f6cfa7c3ec6e1975bb22dfb
azerty9971 <doyen.gilles@gmail.com>
2025-09-24 01:28:44 +0200

    Update __init__.py

--- custom_components/xtend_tuya/__init__.py
+++ custom_components/xtend_tuya/__init__.py
@@ -49,7 +49,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: XTConfigEntry) -> bool:
     start_time = datetime.now()
     multi_manager = MultiManager(hass, entry)
     service_manager = ServiceManager(multi_manager=multi_manager)
-    if entry.title == "USER_INPUT":
+    if entry.title == "USER_INPUT2":
         return True
     await multi_manager.setup_entry()
 
