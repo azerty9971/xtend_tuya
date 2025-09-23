50e0629bc2209994f20d06f94ebe3dc463844fbf
azerty9971 <doyen.gilles@gmail.com>
2025-09-24 01:10:27 +0200

    Update data_entry.py

--- custom_components/xtend_tuya/multi_manager/shared/data_entry/data_entry.py
+++ custom_components/xtend_tuya/multi_manager/shared/data_entry/data_entry.py
@@ -1,20 +1,19 @@
 from __future__ import annotations
 
-from homeassistant.core import (
-    HomeAssistant,
-)
+from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers import (
     discovery_flow,
 )
-from homeassistant.config_entries import (
-    SOURCE_SYSTEM
-)
-from ....const import (
-    DOMAIN
-)
+from homeassistant.config_entries import SOURCE_SYSTEM
+from ....const import DOMAIN
 
 
 def show_test_user_input(hass: HomeAssistant):
+    hass.add_job(_show_test_user_input, hass)
+
+
+@callback
+def _show_test_user_input(hass: HomeAssistant):
     discovery_flow.async_create_flow(
         hass, DOMAIN, context={"source": SOURCE_SYSTEM}, data={}
     )
