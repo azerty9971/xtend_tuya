52b0d74e7560b94cef1d40c47406e111d7d0a895
azerty9971 <doyen.gilles@gmail.com>
2025-09-23 15:52:32 +0200

    Update xt_tuya_iot_device.py

--- custom_components/xtend_tuya/multi_manager/tuya_iot/xt_tuya_iot_device.py
+++ custom_components/xtend_tuya/multi_manager/tuya_iot/xt_tuya_iot_device.py
@@ -18,6 +18,16 @@ class XTSmartHomeDeviceManage(SmartHomeDeviceManage):
                     info.pop("status")
         response["result"]["list"] = response["result"]["devices"]
         return response
+    
+    def get_device_list_status(self, device_ids: list[str]) -> dict[str, Any]:
+        response = self.api.get("/v1.0/devices/", {"device_ids": ",".join(device_ids)})
+        status_list = []
+        if response["success"]:
+            for info in response["result"]["devices"]:
+                status_list.append({"id": info["id"], "status": info.get("status", [])})
+
+        response["result"] = status_list
+        return response
 
 class XTIndustrySolutionDeviceManage(IndustrySolutionDeviceManage):
     pass
