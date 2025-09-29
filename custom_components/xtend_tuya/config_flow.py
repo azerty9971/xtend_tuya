0497a7ffe19720c278a81b8ca2d6170e2d372270
azerty9971 <doyen.gilles@gmail.com>
2025-09-29 19:51:44 +0200

    Update config_flow.py

--- custom_components/xtend_tuya/config_flow.py
+++ custom_components/xtend_tuya/config_flow.py
@@ -260,7 +260,8 @@ class TuyaConfigFlow(ConfigFlow, domain=DOMAIN):
             await self.async_set_unique_id(unique_id=str(uuid.uuid4()))
             self._abort_if_unique_id_configured()
             return self.async_show_form(
-                step_id=user_input.source
+                step_id=user_input.source,
+                data_schema=user_input.schema
             )
         tuya_data = self.hass.config_entries.async_entries(DOMAIN_ORIG, False, False)
         xt_tuya_data = self.hass.config_entries.async_entries(DOMAIN, True, True)
