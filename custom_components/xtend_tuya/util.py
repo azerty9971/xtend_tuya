2ab94df6f0fe16423335e8dd272c4d25eecbebb0
azerty9971 <doyen.gilles@gmail.com>
2025-10-24 05:10:43 +0200

    Update util.py

--- custom_components/xtend_tuya/util.py
+++ custom_components/xtend_tuya/util.py
@@ -292,9 +292,7 @@ def delete_all_device_entities(hass: HomeAssistant, device_ids: list[str], platf
             include_disabled_entities=True,
         )
         for entity_entry in hass_entities:
-            LOGGER.warning(f"Considering removal of {entity_entry.name} entry({entity_entry}) provided platform: {platform}")
             if platform is None or entity_entry.entity_id.startswith(f"{platform}."):
-                LOGGER.warning(f"Removed {entity_entry.name}")
                 entity_registry.async_remove(entity_entry.entity_id)
 
 
