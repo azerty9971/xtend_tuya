cca271ff2dfdef1fd6956d924d52bd27cc08e4ec
azerty9971 <doyen.gilles@gmail.com>
2025-10-30 13:03:03 +0100

    Update config_flow.py

--- custom_components/xtend_tuya/config_flow.py
+++ custom_components/xtend_tuya/config_flow.py
@@ -5,7 +5,8 @@ from collections.abc import Mapping
 from typing import Any, cast
 from enum import StrEnum
 from tuya_sharing import LoginControl
-from tuya_iot import AuthType, TuyaOpenAPI
+from tuya_iot import AuthType
+from multi_manager.tuya_iot.xt_tuya_iot_openapi import XTIOTOpenAPI
 import voluptuous as vol
 from homeassistant.core import callback
 from homeassistant.config_entries import (
@@ -117,7 +118,11 @@ class XTConfigFlows:
             else:
                 data[CONF_AUTH_TYPE] = AuthType.SMART_HOME
 
-            api = TuyaOpenAPI(
+            if XTConcurrencyManager.hass is None:
+                continue
+
+            api = XTIOTOpenAPI(
+                hass=XTConcurrencyManager.hass,
                 endpoint=data[CONF_ENDPOINT_OT],
                 access_id=data[CONF_ACCESS_ID_OT],
                 access_secret=data[CONF_ACCESS_SECRET_OT],
