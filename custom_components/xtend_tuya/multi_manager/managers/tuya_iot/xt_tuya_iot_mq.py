d0dc52721389bfe0601314bed3dcbdaa28c5879c
azerty9971 <doyen.gilles@gmail.com>
2025-11-28 16:08:54 +0100

    Update xt_tuya_iot_mq.py

--- custom_components/xtend_tuya/multi_manager/managers/tuya_iot/xt_tuya_iot_mq.py
+++ custom_components/xtend_tuya/multi_manager/managers/tuya_iot/xt_tuya_iot_mq.py
@@ -1,21 +1,9 @@
 from __future__ import annotations
-from typing import Any
-import uuid
-from paho.mqtt import client as mqtt
-from urllib.parse import urlsplit
 from ....lib.tuya_iot import (
     TuyaOpenMQ,
 )
 from ....lib.tuya_iot.openmq import (
     TuyaMQConfig,
-    TO_C_CUSTOM_MQTT_CONFIG_API,
-    AuthType,
-    TO_C_SMART_HOME_MQTT_CONFIG_API,
-    CONNECT_FAILED_NOT_AUTHORISED,
-    time,
-)
-from ....const import (
-    LOGGER,
 )
 from .xt_tuya_iot_openapi import (
     XTIOTOpenAPI,
