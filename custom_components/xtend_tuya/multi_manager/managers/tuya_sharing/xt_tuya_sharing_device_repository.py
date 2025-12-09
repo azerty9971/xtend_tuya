e543b29f7076551b7980c9b2b24a53569a0019ac
azerty9971 <doyen.gilles@gmail.com>
2025-12-09 11:10:07 +0100

    Update xt_tuya_sharing_device_repository.py

--- custom_components/xtend_tuya/multi_manager/managers/tuya_sharing/xt_tuya_sharing_device_repository.py
+++ custom_components/xtend_tuya/multi_manager/managers/tuya_sharing/xt_tuya_sharing_device_repository.py
@@ -55,6 +55,8 @@ class XTSharingDeviceRepository(DeviceRepository):
                 dpcode = None
                 if hasattr(value, "code"):
                     dpcode = str(getattr(value, "code"))
+                if hasattr(value, "type") and isinstance(getattr(value, "type"), str):
+                    setattr(device.function[key], "type", str(getattr(value, "type")).lower())
                 values = None
                 if hasattr(value, "values"):
                     values = getattr(value, "values")
