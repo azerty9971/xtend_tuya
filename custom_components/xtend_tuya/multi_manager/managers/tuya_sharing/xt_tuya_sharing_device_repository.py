ca5deddd3ea33abc95327ee0be665a7fc5ea10dc
azerty9971 <doyen.gilles@gmail.com>
2025-12-09 15:05:07 +0100

    Update xt_tuya_sharing_device_repository.py

--- custom_components/xtend_tuya/multi_manager/managers/tuya_sharing/xt_tuya_sharing_device_repository.py
+++ custom_components/xtend_tuya/multi_manager/managers/tuya_sharing/xt_tuya_sharing_device_repository.py
@@ -50,6 +50,7 @@ class XTSharingDeviceRepository(DeviceRepository):
                     if dpcode is not None and dpcode == key:
                         #This is an infrared dpcode, fix the values to be proper json
                         setattr(device.status_range[key], "values", json.dumps({"original_content": values}))
+                        device.status[key] = False
             
             for key, value in device.function.items():
                 dpcode = None
@@ -63,6 +64,7 @@ class XTSharingDeviceRepository(DeviceRepository):
                     if dpcode is not None and dpcode == key:
                         #This is an infrared dpcode, fix the values to be proper json
                         setattr(device.function[key], "values", json.dumps({"original_content": values}))
+                        device.status[key] = False
 
     def update_device_specification(self, device: CustomerDevice):
         super().update_device_specification(device)
