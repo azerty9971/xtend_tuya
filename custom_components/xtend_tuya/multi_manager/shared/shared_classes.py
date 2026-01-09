feafaab2f6d5c1e883b68c07fad0367605e25b9e
azerty9971 <doyen.gilles@gmail.com>
2026-01-10 00:01:19 +0100

    Merge common interface in XTDeviceFunction and XTDeviceStatusRange

--- custom_components/xtend_tuya/multi_manager/shared/shared_classes.py
+++ custom_components/xtend_tuya/multi_manager/shared/shared_classes.py
@@ -72,14 +72,15 @@ class HomeAssistantXTData(NamedTuple):
 
 type XTConfigEntry = ConfigEntry[HomeAssistantXTData]
 
-
 @dataclass
-class XTDeviceStatusRange:
+class XTDeviceStatusFunctionShared:
     code: str = ""
     type: TuyaDPType | None = None
     values: str = "{}"
     dp_id: int = 0
 
+@dataclass
+class XTDeviceStatusRange(XTDeviceStatusFunctionShared):
     def __repr__(self) -> str:
         return f"StatusRange(code={self.code}, type={self.type}, values={self.values}, dp_id={self.dp_id})"
 
@@ -105,13 +106,9 @@ class XTDeviceStatusRange:
 
 
 @dataclass
-class XTDeviceFunction:
-    code: str = ""
-    type: TuyaDPType | None = None
+class XTDeviceFunction(XTDeviceStatusFunctionShared):
     desc: str = ""
     name: str = ""
-    values: str = "{}"
-    dp_id: int = 0
 
     def __repr__(self) -> str:
         return f"Function(code={self.code}, type={self.type}, desc={self.desc}, name={self.name}, values={self.values}, dp_id={self.dp_id})"
