8866c94975fad0317e6d61146488ad26f03c5b55
azerty9971 <3135352+azerty9971@users.noreply.github.com>
2026-03-12 21:15:48 +0100

    Update xt_tuya_iot_manager.py

--- custom_components/xtend_tuya/multi_manager/managers/tuya_iot/xt_tuya_iot_manager.py
+++ custom_components/xtend_tuya/multi_manager/managers/tuya_iot/xt_tuya_iot_manager.py
@@ -301,9 +301,21 @@ class XTIOTDeviceManager(TuyaDeviceManager):
         status_new = self.multi_manager.convert_device_report_status_list(
             device_id, status
         )
+        # report_value = False
+        for status_item in status_new:
+            if code := status_item.get("code", None):
+                if code == "add_ele":
+                    # report_value = True
+                    LOGGER.warning(
+                        f"[{MESSAGE_SOURCE_TUYA_IOT}]Device {device_id} reported add_ele: {status_new}, before filtering"
+                    )
         status_new = self.multi_manager.multi_source_handler.filter_status_list(
             device_id, MESSAGE_SOURCE_TUYA_IOT, status_new
         )
+        # if report_value:
+        #     LOGGER.warning(
+        #         f"[{MESSAGE_SOURCE_TUYA_IOT}]Device {device_id} reported add_ele: {status_new}, after filtering"
+        #     )
         status_new = self.multi_manager.virtual_state_handler.apply_virtual_states_to_status_list(
             device, status_new, MESSAGE_SOURCE_TUYA_IOT
         )
