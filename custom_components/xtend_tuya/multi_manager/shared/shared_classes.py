00f7014ea2c75c44ea6dae917012472d500bf862
azerty9971 <3135352+azerty9971@users.noreply.github.com>
2026-05-19 16:01:16 +0200

    Update shared_classes.py

--- custom_components/xtend_tuya/multi_manager/shared/shared_classes.py
+++ custom_components/xtend_tuya/multi_manager/shared/shared_classes.py
@@ -52,7 +52,7 @@ class DeviceWatcher:
             # "bf022344b6e0cfd5dafh8e": XTDeviceWatcherCategory.MQTT,
             # XTDeviceWatcherSpecialDevice.NOT_LINKED_TO_A_DEVICE: XTDeviceWatcherCategory.IOT_API,
             # "vdevo172985271302839": XTDeviceWatcherCategory.PLATFORM_LOCK,
-            # "ebf857472d1a0bd943jqag": XTDeviceWatcherCategory.MQTT | XTDeviceWatcherCategory.IOT_API | XTDeviceWatcherCategory.SHARING_API | XTDeviceWatcherCategory.STATUS_CHANGES
+            "ebf857472d1a0bd943jqag": XTDeviceWatcherCategory.MQTT | XTDeviceWatcherCategory.IOT_API | XTDeviceWatcherCategory.SHARING_API | XTDeviceWatcherCategory.STATUS_CHANGES
         }
         self.multi_manager = multi_manager
 
