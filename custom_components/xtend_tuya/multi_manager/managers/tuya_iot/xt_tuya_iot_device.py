2cd902b8f02500772166ab54be3b67055a09cb21
azerty9971 <3135352+azerty9971@users.noreply.github.com>
2026-09-07 00:13:40 +0200

    Update xt_tuya_iot_device.py

--- custom_components/xtend_tuya/multi_manager/managers/tuya_iot/xt_tuya_iot_device.py
+++ custom_components/xtend_tuya/multi_manager/managers/tuya_iot/xt_tuya_iot_device.py
@@ -1,14 +1,33 @@
 from __future__ import annotations
 
+from typing import Any
+
 from ....lib.tuya_iot.device import (
     SmartHomeDeviceManage,
     IndustrySolutionDeviceManage,
 )
+from .xt_tuya_iot_openapi import (
+    XTIOTOpenAPI,
+)
 
 
 class XTSmartHomeDeviceManage(SmartHomeDeviceManage):
-    pass
+    api: XTIOTOpenAPI # type: ignore
+    
+    def __init__(self, api: XTIOTOpenAPI):
+        super().__init__(api=api)
+
+    # https://developer.tuya.com/en/docs/cloud/device-control?id=K95zu01ksols7#title-27-Get%20the%20specifications%20and%20properties%20of%20the%20device%2C%20including%20the%20instruction%20set%20and%20status%20set
+    def get_device_specification(self, device_id: str) -> dict[str, Any]:
+        return self.api.get(f"/v1.0/devices/{device_id}/specifications", allow_caching=True)
 
 
 class XTIndustrySolutionDeviceManage(IndustrySolutionDeviceManage):
-    pass
+    api: XTIOTOpenAPI # type: ignore
+        
+    def __init__(self, api: XTIOTOpenAPI):
+        super().__init__(api=api)
+
+    # https://developer.tuya.com/en/docs/cloud/device-control?id=K95zu01ksols7#title-27-Get%20the%20specifications%20and%20properties%20of%20the%20device%2C%20including%20the%20instruction%20set%20and%20status%20set
+    def get_device_specification(self, device_id: str) -> dict[str, str]:
+        return self.api.get(f"/v1.0/iot-03/devices/{device_id}/specification", allow_caching=True)
