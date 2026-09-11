e5131003f94976b9bd365881e6ebd4b0ff1191f0
azerty9971 <3135352+azerty9971@users.noreply.github.com>
2026-09-11 19:26:50 +0200

    Add request caching for Tuya IOT

--- custom_components/xtend_tuya/multi_manager/managers/tuya_iot/xt_tuya_iot_openapi.py
+++ custom_components/xtend_tuya/multi_manager/managers/tuya_iot/xt_tuya_iot_openapi.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Any
 import time
+import json
 
 from custom_components.xtend_tuya.lib.tuya_iot.openapi import TuyaTokenInfo
 from custom_components.xtend_tuya.lib.tuya_iot.tuya_enums import AuthType
@@ -15,6 +16,10 @@ from ....const import (
     XTDeviceWatcherSpecialDevice,
     XTDeviceWatcherCategory,
     LOGGER,
+    MESSAGE_SOURCE_TUYA_IOT,
+)
+from ...shared.threading import (
+    XTEventLoopProtector,
 )
 
 if TYPE_CHECKING:
@@ -61,8 +66,55 @@ class XTIOTOpenAPI(TuyaOpenAPI):
         else:
             return super().report_message(method, message, stack_info)
 
-    def _register_request_in_log(self, method: str, path:str, params: dict[str, Any] | None, caching_allowed: bool, elapsed_time: float, group_key: str | None):
+    def _register_request_in_log(
+        self,
+        method: str,
+        path: str,
+        params: dict[str, Any] | None,
+        caching_allowed: bool,
+        elapsed_time: float,
+    ):
         self.request_log.append(f"{method} {path} | {caching_allowed} | {elapsed_time}")
+
+    def _get_request_from_cache(
+        self, method: str, path: str, params: dict[str, Any] | None
+    ) -> dict[str, Any] | None:
+        if self.multi_manager is None:
+            return None
+        prop_name = f"{method} {path}"
+        if params is not None:
+            prop_name += f" {json.dumps(params)}"
+        prop_value = (
+            self.multi_manager.storage_manager.get_device_configurable_property(
+                device_id=MESSAGE_SOURCE_TUYA_IOT,
+                dpcode="API_REQUEST_CACHE",
+                prop_name=prop_name,
+            )
+        )
+        if prop_value is not None and isinstance(prop_value, str):
+            return json.loads(prop_value)
+
+    def _save_request_to_cache(
+        self,
+        method: str,
+        path: str,
+        params: dict[str, Any] | None,
+        payload: dict[str, Any],
+    ):
+        if self.multi_manager is None:
+            return None
+        prop_name = f"{method} {path}"
+        if params is not None:
+            prop_name += f" {json.dumps(params)}"
+        self.multi_manager.storage_manager.set_device_configurable_property(
+            device_id=MESSAGE_SOURCE_TUYA_IOT,
+            dpcode="API_REQUEST_CACHE",
+            prop_name=prop_name,
+            prop_value=json.dumps(payload),
+        )
+        XTEventLoopProtector.execute_out_of_event_loop(
+            self.multi_manager.storage_manager.save_store
+        )
 
     def get_request_log(self) -> list[str]:
         return self.request_log
@@ -73,21 +125,48 @@ class XTIOTOpenAPI(TuyaOpenAPI):
             request_string += f"{request}\n\r"
         LOGGER.debug(f"print_request_log\n\r{request_string}")
 
-    def get(self, path: str, params: dict[str, Any] | None = None, allow_caching: bool = False, group_key: str | None = None) -> dict[str, Any]:
+    def get(
+        self,
+        path: str,
+        params: dict[str, Any] | None = None,
+        allow_caching: bool = False,
+    ) -> dict[str, Any]:
+
+        to_return = None
+        if allow_caching:
+            to_return = self._get_request_from_cache(
+                method="GET",
+                path=path,
+                params=params,
+            )
         start = time.perf_counter()
-
-        to_return = super().get(path=path, params=params)
-
+        if to_return is None:
+            to_return = super().get(path=path, params=params)
         elapsed = time.perf_counter() - start
-        self._register_request_in_log(method="GET", path=path, params=params, caching_allowed=allow_caching, elapsed_time=elapsed, group_key=group_key)
+        self._register_request_in_log(
+            method="GET",
+            path=path,
+            params=params,
+            caching_allowed=allow_caching,
+            elapsed_time=elapsed,
+        )
+        if allow_caching:
+            self._save_request_to_cache(
+                method="GET", path=path, params=params, payload=to_return
+            )
         return to_return
 
-    def post(self, path: str, body: dict[str, Any] | None = None, group_key: str | None = None) -> dict[str, Any]:
+    def post(self, path: str, body: dict[str, Any] | None = None) -> dict[str, Any]:
         start = time.perf_counter()
 
         to_return = super().post(path=path, body=body)
 
         elapsed = time.perf_counter() - start
-        self._register_request_in_log(method="POST", path=path, params=body, caching_allowed=False, elapsed_time=elapsed, group_key=group_key)
+        self._register_request_in_log(
+            method="POST",
+            path=path,
+            params=body,
+            caching_allowed=False,
+            elapsed_time=elapsed,
+        )
         return to_return
-        
